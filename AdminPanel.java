d6563b37153f70e73a3089dd24a854071f6ce25e
Megan <mjoo@cpp.edu>
2023-11-14 17:23:40 -0800

    UI layout

--- AdminPanel.java
+++ AdminPanel.java
@@ -23,8 +23,8 @@ public class AdminPanel {
 		JButton groupTotal = new JButton("Show Group Total");
 		JButton msgsTotal = new JButton("Show Message Total");
 		JButton positivePercent = new JButton("Show Positive Percentage");
-		JTextArea userId = new JTextArea("Enter user ID");
-		JTextArea groupId = new JTextArea("Enter group ID");
+		JTextArea userId = new JTextArea();
+		JTextArea groupId = new JTextArea();
 
 		JPanel left = new JPanel();
 		JPanel right = new JPanel();
@@ -34,6 +34,10 @@ public class AdminPanel {
 		left.setLayout(new BorderLayout());
 		right.setLayout(new GridLayout(2, 0));
 		bottom.setLayout(new GridLayout(0,2));
+		top.setLayout(new GridBagLayout());
+
+		bottom.setBackground(Color.CYAN);
+		top.setBackground(Color.CYAN);
 
 		//Tree view for users/groups
 		UserGroup root = new UserGroup("Root");
@@ -41,14 +45,137 @@ public class AdminPanel {
 		rootNode.setUserObject(root);
 		DefaultTreeModel treeModel = new DefaultTreeModel(rootNode);
 		JTree tree = new JTree(treeModel);
-		JScrollPane scrlp = new JScrollPane();
-		scrlp.add(tree);
-		left.add(scrlp);
+		JScrollPane scrlp = new JScrollPane(tree);
+		left.add(scrlp, BorderLayout.CENTER);
 
-		top.add(userId);
-		top.add(groupId);
-		top.add(addUser);
-		top.add(addGroup);
+		//Listener to add user when button is pressed
+		addUser.addActionListener(ae -> {
+			DefaultMutableTreeNode selectedUser = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+			if (selectedUser == null) {
+				selectedUser = rootNode;
+			}
+			User newUser = new User(userId.getText());
+			DefaultMutableTreeNode newUserInput = new DefaultMutableTreeNode(newUser);
+			if (selectedUser.getUserObject() instanceof User) {
+				JOptionPane.showMessageDialog(frame, "Cannot add user to user!");
+			} 
+			else if (root.containsUser(userId.getText())) {
+				JOptionPane.showMessageDialog(frame, "User already exists!");
+			} 
+			else {
+				selectedUser.add(newUserInput);
+				((UserGroup) selectedUser.getUserObject()).addUsers(newUser);
+			}
+			
+			treeModel.reload();
+		});
+
+		//Listener to add group when button is pressed
+		addGroup.addActionListener(ae -> {
+			DefaultMutableTreeNode selectedUser = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+			if (selectedUser == null) {
+				selectedUser = rootNode;
+			}
+			UserGroup newGroup = new UserGroup(groupId.getText());
+			DefaultMutableTreeNode newGroupInput = new DefaultMutableTreeNode(newGroup);
+			System.out.println(root.containsGroup(groupId.getText()) + " " + groupId.getText());
+			if (selectedUser.getUserObject() instanceof User) {
+				JOptionPane.showMessageDialog(frame, "Cannot add group to user!");
+			} else if (root.containsGroup(groupId.getText())) {
+				JOptionPane.showMessageDialog(frame, "Group already exists!");
+			} else {
+				selectedUser.add(newGroupInput);
+				((UserGroup) selectedUser.getUserObject()).addUsers(newGroup);
+			}
+			treeModel.reload();
+		});
+
+		userView.addActionListener(ae -> {
+			DefaultMutableTreeNode selectedUser = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+			if (selectedUser.getUserObject() instanceof User) {
+				User userViewUser = (User) selectedUser.getUserObject();
+				UserView.openUserView(frame, userViewUser, root);
+			}
+			 
+		});
+
+		//Listener to display stats for buttons
+		userTotal.addActionListener(ae -> {
+			TotalUserVisitor userVis = new TotalUserVisitor();
+			root.accept(userVis);
+			JOptionPane.showMessageDialog(frame, "Total Users: " + userVis.getUserTotal() + " users total", "Total Users", 0);
+		});
+
+		groupTotal.addActionListener(ae -> {
+			TotalGroupVisitor groupVis = new TotalGroupVisitor();
+			root.accept(groupVis);
+			JOptionPane.showMessageDialog(frame, "Total Groups: " + groupVis.getTotalGroups(), "Total Groups", 0);
+		});
+
+		msgsTotal.addActionListener(ae -> {
+			TotalTweetsVisitor twtsVis = new TotalTweetsVisitor();
+			root.accept(twtsVis);
+			JOptionPane.showMessageDialog(frame, "Total Tweets: " + twtsVis.getTotalTweets(), "Total Tweets", 0);
+		});
+
+		positivePercent.addActionListener(ae -> {
+			PositiveTweetsVisitor posVis = new PositiveTweetsVisitor();
+			root.accept(posVis);
+			JOptionPane.showMessageDialog(frame, "Percentage of Positive Tweets: " + posVis.getPositivePercentage(), "Positive Percentage", 0);
+		});
+
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL; 
+
+		JLabel username = new JLabel("Enter User ID: ");
+		c.gridx = 0;		//1st column
+		c.gridy = 0;		//1st row
+		c.ipady = 20;
+		top.add(username, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;		//2nd column
+		c.gridy = 0;		//1st row
+		top.add(userId, c);
+
+		JLabel groupLabel = new JLabel("Enter Group ID: ");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 2;		//3rd column
+		c.gridy = 0;		//1st row
+		c.weighty = 1;
+		top.add(groupLabel, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 3;		//4th column
+		c.gridy = 0;		//1st row
+		c.weighty = 1;
+		top.add(groupId, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridwidth = 2;	//2 columns wide
+		c.gridx = 0;		//1st column
+		c.gridy = 1;		//2nd row
+		c.ipady = 20;
+		c.ipady = 25;
+		c.weighty = 0.25;		
+		top.add(addUser, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 2;		//2nd column
+		c.gridy = 1;		//2nd row
+		c.gridwidth = 2;	//2 columns wide
+		c.ipady = 25;
+		c.weighty = 0.25;
+		top.add(addGroup, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.ipady = 40;      //make this component tall
+		c.weighty = 0.25;
+		c.gridwidth = 4;	
+		c.gridx = 0;		//1st column
+		c.gridy = 2;		//3rd row
+		top.add(userView, c);
 
 		bottom.add(userTotal);
 		bottom.add(groupTotal);
@@ -60,6 +187,9 @@ public class AdminPanel {
 
 		frame.add(left);
 		frame.add(right);
+
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
 	public static AdminPanel getInstance() {
--- UserView.java
+++ UserView.java
@@ -6,20 +6,21 @@ public class UserView {
 	static void openUserView(JFrame frame, User user, UserGroup root) {
 		JDialog userViewArea = new JDialog(frame, user.getId(), false);
 		userViewArea.setSize(350, 300);
-		userViewArea.setLayout(new FlowLayout());
+		userViewArea.setLayout(new GridBagLayout());
+		userViewArea.setBackground(Color.CYAN);
+		userViewArea.setForeground(Color.CYAN);
 		userViewArea.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		JLabel username = new JLabel(user.getId());
 		JTextArea userId = new JTextArea("User Id");
 		JButton follow = new JButton("Follow User");
 		JTextArea tweet = new JTextArea("What's happening?",5, 20);
 		JButton post = new JButton("Post Tweet");
 		JList followerList = new JList<>(user.getFollowings());
 		JList newsFeed = new JList<>(user.getNewsFeed());
+		
 
 		follow.addActionListener(ae -> {
 			String id = userId.getText();
-			//GetUser finds if a user exists
-			//User userToFollow = 
+			User userToFollow = root.getUser(id);
 			if (userToFollow == null) {
 				JOptionPane.showMessageDialog(userViewArea, "User does not exist!");
 			}
@@ -34,13 +35,47 @@ public class UserView {
 			user.notifyObservers(message);
 		});
 
-		userViewArea.add(username);
-		userViewArea.add(userId);
-		userViewArea.add(follow);
-		userViewArea.add(tweet);
-		userViewArea.add(post);
-		userViewArea.add(newsFeed);
-		userViewArea.add(followerList);
+
+
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL; 
+		c.gridx = 0;		//1st column
+		c.gridy = 0;		//1st row
+		c.weightx = 0.5;
+		c.weighty = 1;
+		userViewArea.add(userId, c);
+
+		c.gridx = 1;
+		c.gridy = 0;
+		userViewArea.add(follow, c);
+
+		c.ipady = 30;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.weightx = 0.5;
+		c.gridwidth = 2;
+		userViewArea.add(followerList, c);
+
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 2;
+		c.weightx = 0.5;
+		c.ipady = 30;
+		userViewArea.add(tweet, c);
+
+		c.gridx = 1;
+		c.gridy = 2;
+		c.weightx = 0.5;
+		userViewArea.add(post, c);
+
+		c.gridx = 0;
+		c.gridy = 3;
+		c.gridwidth = 2;
+		userViewArea.add(newsFeed, c);
+
+
+
+
 
 		userViewArea.setLocationRelativeTo(frame);
 		userViewArea.setVisible(true);
